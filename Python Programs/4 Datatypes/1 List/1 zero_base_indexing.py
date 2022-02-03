757c644c8d846834d317a7b477e14ef9a91d6b75
Rohit Paritkar <96126072+RohitParitkar@users.noreply.github.com>
2022-02-03 16:39:31 +0530

    Create 1 zero_base_indexing.py

---
+++ Python Programs/4 Datatypes/1 List/1 zero_base_indexing.py
@@ -0,0 +1,19 @@
+# my_list=[10,2.9,56,'rohit',62,1098]
+# #access list by zero base indexing 
+# print(my_list[2])
+
+
+# ######### using for loop ##########
+
+# for i in range (len(my_list)):
+#     print(my_list[i])
+
+# ######## for each loop ########
+# print("list: ",my_list)
+# for my_list[2] in my_list:
+#     print(my_list)
+
+mylist= [1,2,3,4]
+for mylist[2] in mylist:
+    print(mylist)
+print("The Answer: ",mylist)
