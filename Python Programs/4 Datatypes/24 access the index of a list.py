cfba6f780871f058c119497d59a2e474b0aa6bb2
Rohit Paritkar <96126072+RohitParitkar@users.noreply.github.com>
2022-02-03 16:43:52 +0530

    Delete Python Programs/4 Datatypes directory

--- Python Programs/4 Datatypes/24 access the index of a list.py
+++
@@ -1,5 +0,0 @@
-# 20. Write a Python program access the index of a list. 
-mylist=[1,2,3,4,5,6]
-for i in range (len(mylist)):
-    print(i,mylist[i])
-    i=i+1
--- Python Programs/4 Datatypes/30 two lists are circularly identical.py
+++
@@ -1,1 +0,0 @@
-# 26. Write a python program to check whether two lists are circularly identical. 
