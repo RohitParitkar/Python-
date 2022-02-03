64abf1e4f31130ba6c002099221d65a4caca42ad
Rohit Paritkar <96126072+RohitParitkar@users.noreply.github.com>
2022-02-03 16:52:29 +0530

    Create packing and unpacking.py

---
+++ Python Programs/4 Datatypes/3 tuple/packing and unpacking.py
@@ -0,0 +1,16 @@
+#packing
+t1=(10,20,30)
+#unpacking
+(a,b,c)=t1
+print(a)
+print(b)
+print(c)
+
+################
+#creating tuple with only one value.
+t2=(10,)
+print(type(t2))
+t3=10,20,30,40
+print(type(t3))
+t4= 10, 
+print(type(t4))
