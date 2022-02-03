432ff55524b78d47ebd7ee5e7a099233225d32cd
Rohit Paritkar <96126072+RohitParitkar@users.noreply.github.com>
2022-02-03 16:50:36 +0530

    Create 1 simple sort.py

---
+++ Python Programs/4 Datatypes/2 sort/1 simple sort.py
@@ -0,0 +1,7 @@
+mysort=[1980,230,30,240,5,610,70]
+print("before sorting",mysort)
+mysort.sort()
+
+print("after sorting",mysort)
+mysort.sort(reverse=True)
+print("after sorting in reverse: ",mysort)
