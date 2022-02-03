57750aadea6ffab5e34c08e61dddb7e4a327c49f
Rohit Paritkar <96126072+RohitParitkar@users.noreply.github.com>
2022-02-03 16:03:15 +0530

    Add files via upload

---
+++ python program/basic operators/11 Convert meter to Feet and Inches.py
@@ -0,0 +1,10 @@
+# 9. Write a Program to Convert meter to Feet and Inches
+# 1 meter=12 inches
+# 1 meter=100 centimeters
+# 1 meter= 3.28084 Feet
+
+meter = float(input("enter the value of meter: "))
+feet= meter*3.28084
+print("conversion of meter to feet is",feet,"Feet.")
+inches = meter*12
+print("conversion of meter to inches is",inches,"inches.")
